cb3a0f92a2fbb4dd7cde8b37671a17773c14c160
Jakub Rozycki <dev@jrozycki.me>
2023-02-21 22:29:38 +0100

    Change removed comm prints, conv nparr to list

--- bubbleSort.py
+++ bubbleSort.py
@@ -3,7 +3,7 @@ import numpy as np
 class BubbleSort:
     def __init__(self, data):
         self.data = data
-        # print(f"Data to be sorted: {self.data}")
+
 
     def sort(self):
         while True:
@@ -14,9 +14,9 @@ class BubbleSort:
                     finished = False
             if finished:
                 break
-        # print(f"Sorted data:       {self.data}")
+
 
 if __name__ == "__main__":
-    data_random = np.random.randint(1000, size=(1000))
+    data_random = np.random.randint(1000, size=(1000)).tolist()
     BS = BubbleSort(data_random)
     BS.sort()
--- insertionSort.py
+++ insertionSort.py
@@ -3,7 +3,6 @@ import numpy as np
 class InsertionSort:
     def __init__(self, data):
         self.data = data
-        # print(f"Data to be sorted: {self.data}")
 
     def sort(self):
         for number in range(1, len(self.data)):
@@ -12,11 +11,8 @@ class InsertionSort:
                 self.data[index], self.data[index-1] = self.data[index-1], self.data[index]
                 index -= 1
 
-        # print(f"Sorted data:       {self.data}")
-
-
 
 if __name__ == "__main__":
-    data_random = np.random.randint(1000, size=(1000))
+    data_random = np.random.randint(1000, size=(1000)).tolist()
     IS = InsertionSort(data_random)
     IS.sort()
