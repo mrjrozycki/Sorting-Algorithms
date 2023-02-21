f9b41dbf5e837924883ee02db0bfff3216cc2710
Jakub Rozycki <dev@jrozycki.me>
2023-02-21 20:38:09 +0100

    Add Bubble sort created

---
+++ bubbleSort.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class BubbleSort:
+    def __init__(self, data):
+        self.data = data
+        print(f"Data to be sorted: {self.data}")
+
+    def sort(self):
+        while True:
+            finished = True
+            for number in range(len(self.data)-1):
+                if self.data[number] > self.data[number+1]:
+                    self.data[number], self.data[number+1] = self.data[number+1], self.data[number]
+                    finished = False
+            if finished:
+                break
+        print(f"Sorted data:       {self.data}")
+
+if __name__ == "__main__":
+    data_random = np.random.randint(10000, size=(10000))
+    BS = BubbleSort(data_random)
+    BS.sort()
